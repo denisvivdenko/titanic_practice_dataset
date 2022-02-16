fec0e142c0f93936d8d210914795d4d000aafce2
denisvivdenko <denis.vivdenko.freelancer@gmail.com>
2022-02-16 18:51:04 +0200

    feat: add uknown value for empty str

--- alphabetic_code_extractor.py
+++ alphabetic_code_extractor.py
@@ -4,21 +4,22 @@ from sklearn.preprocessing import FunctionTransformer
 @FunctionTransformer
 def extract_alphabetic_code_by_split(X: np.array, split_value: str = " ") -> np.array:
     extract_code = np.vectorize(lambda x: " ".join([code for code in x.split(split_value) if not code.isnumeric()]))
-    return extract_code(X)
+    extracted_codes = extract_code(X)
+    return np.where(extracted_codes == "", "unknown", extracted_codes)
 
 @FunctionTransformer
 def extract_alphabetic_code(X: np.array) -> np.array:
     extract_alphabetic = np.vectorize(lambda x: ''.join(filter(str.isalpha, str(x))))
-    return extract_alphabetic(X)
-
+    extracted_codes = extract_alphabetic(X)
+    return np.where(extracted_codes == "", "unknown", extracted_codes)
 
 if __name__ == "__main__":
-    print(extract_alphabetic_code_by_split.fit_transform(np.array(["B1C 12", "AB 3", "K 1"])))
+    print(extract_alphabetic_code_by_split.fit_transform(np.array(["B1C 12", "AB 3", "K 1", "12"])))
     print(extract_alphabetic_code.fit_transform(np.array(["B1C 12", "AB 3B", "K 121C"])))
 
 """
-input: ["B1C 12", "AB 3", "K 1"]
-output: ['B1C' 'AB' 'K']
+input: ["B1C 12", "AB 3", "K 1", "12"]
+output: ['B1C' 'AB' 'K', 'unknown']
 
 input: ["B1C 12", "AB 3B", "K 121C"]
 output: ['BC' 'ABB' 'KC']
